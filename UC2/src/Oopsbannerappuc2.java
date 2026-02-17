0c0059a894d73a7ea0dc9547209a2e6f0a5ebb7f
rk8141-cmyk <rk8141@srmist.edu.in>
2026-02-17 13:32:13 +0530

    Development

---
+++ UC2/src/Oopsbannerappuc2.java
@@ -0,0 +1,11 @@
+public class Oopsbannerappuc2{
+    public static void main(String[] args){
+        System.out.println(" *****    *****    ******    *****  ");
+        System.out.println("*     *  *     *   *     *  *     * ");
+        System.out.println("*     *  *     *   *     *  *       ");
+        System.out.println("*     *  *     *   ******    *****  ");
+        System.out.println("*     *  *     *   *              * ");
+        System.out.println("*     *  *     *   *        *     * ");
+        System.out.println(" *****    *****    *         *****  ");
+    }
+}
---
+++ UC4/src/OOPSBannerUC4.java
@@ -0,0 +1,21 @@
+public class OOPSBannerUC4 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+                String.join("", " *****  ", " *****  ", " *****  ", " ***** "),
+                String.join("", "*     * ", "*     * ", "*    *  ", "*     *"),
+                String.join("", "*     * ", "*     * ", "*    *  ", "*      "),
+                String.join("", "*     * ", "*     * ", "******  ", "*******"),
+                String.join("", "*     * ", "*     * ", "*       ", "      *"),
+                String.join("", "*     * ", "*     * ", "*       ", " *     *"),
+                String.join("", " *****  ", " *****  ", "*       ", " ***** ")
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
